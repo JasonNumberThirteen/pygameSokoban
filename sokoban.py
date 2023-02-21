dc646734f51d5fd9bf014723dc282739a1a4ec3e
Jason <numberthirteen4026@gmail.com>
2023-02-21 17:02:36 +0100

    Add a tile drawing

--- sokoban.py
+++ sokoban.py
@@ -3,6 +3,8 @@ import pygame
 GAME_TITLE = "Sokoban"
 BASE_RESOLUTION = (640, 480)
 BACKGROUND_COLOR = (224, 224, 224)
+TILE_COLOR = (32, 32, 64)
+TILE_BORDER_RADIUS = 8
 
 pygame.init()
 pygame.display.set_caption(GAME_TITLE)
@@ -12,6 +14,7 @@ is_running = True
 
 while is_running:
 	canvas.fill(BACKGROUND_COLOR)
+	pygame.draw.rect(canvas, TILE_COLOR, pygame.Rect(32, 32, 32, 32), 0, TILE_BORDER_RADIUS)
 	
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
