c56b357f77918a3f452e8c1416084a1427ccb153
Jason <numberthirteen4026@gmail.com>
2023-02-21 14:00:18 +0100

    Initial commit

---
+++ sokoban.py
@@ -0,0 +1,19 @@
+import pygame
+
+GAME_TITLE = "Sokoban"
+BASE_RESOLUTION = (256, 144)
+
+pygame.init()
+pygame.display.set_caption(GAME_TITLE)
+
+canvas = pygame.display.set_mode(BASE_RESOLUTION)
+is_running = True
+
+while is_running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            is_running = False
+
+    pygame.display.update()
+
+pygame.quit()
