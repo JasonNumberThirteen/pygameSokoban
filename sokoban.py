f6e78865b55d50ec7e56475af44e9b7d5910fb6a
Jason <numberthirteen4026@gmail.com>
2023-02-21 16:53:15 +0100

    Set the background color

--- sokoban.py
+++ sokoban.py
@@ -2,6 +2,7 @@ import pygame
 
 GAME_TITLE = "Sokoban"
 BASE_RESOLUTION = (640, 480)
+BACKGROUND_COLOR = (224, 224, 224)
 
 pygame.init()
 pygame.display.set_caption(GAME_TITLE)
@@ -10,10 +11,12 @@ canvas = pygame.display.set_mode(BASE_RESOLUTION)
 is_running = True
 
 while is_running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            is_running = False
+	canvas.fill(BACKGROUND_COLOR)
+	
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			is_running = False
 
-    pygame.display.update()
+	pygame.display.update()
 
 pygame.quit()
