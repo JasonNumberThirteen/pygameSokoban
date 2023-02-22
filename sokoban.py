50d69da6eccfb832e7b083959a9d1584cb90ac7b
Jason <numberthirteen4026@gmail.com>
2023-02-22 14:20:27 +0100

    Add a __name__ condition in the main module

--- sokoban.py
+++ sokoban.py
@@ -2,24 +2,28 @@ import pygame
 import level
 import constants
 
-pygame.init()
-pygame.display.set_caption(constants.GAME_TITLE)
+def main():
+	pygame.init()
+	pygame.display.set_caption(constants.GAME_TITLE)
 
-canvas = pygame.display.set_mode(constants.BASE_RESOLUTION)
-is_running = True
-level_data = [[0, 1, 0, 1, 1], [1, 1, 4, 1, 3], [0, 0, 1, 1, 2]]
-level = level.Level(level_data)
+	canvas = pygame.display.set_mode(constants.BASE_RESOLUTION)
+	is_running = True
+	level_data = [[0, 1, 0, 1, 1], [1, 1, 4, 1, 3], [0, 0, 1, 1, 2]]
+	level_one = level.Level(level_data)
 
-while is_running:
-	canvas.fill(constants.BACKGROUND_COLOR)
-	level.draw(canvas)
+	while is_running:
+		canvas.fill(constants.BACKGROUND_COLOR)
+		level_one.draw(canvas)
 
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
-			is_running = False
-		
-		level.player.detect_input(event)
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				is_running = False
+			
+			level_one.player.detect_input(event)
 
-	pygame.display.update()
+		pygame.display.update()
 
-pygame.quit()
+	pygame.quit()
+
+if __name__ == "__main__":
+	main()
