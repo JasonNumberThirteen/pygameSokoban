a3b6866ee1904d7ba4a25bab32d2a9ff9a89b90c
Jason <numberthirteen4026@gmail.com>
2023-02-22 16:18:17 +0100

    Add a method changing UI text label

--- text.py
+++ text.py
@@ -2,10 +2,15 @@ import pygame
 
 class Text(object):
 	def __init__(self, text, x=0, y=0, color=(255, 255, 255), font_size=32):
-		self.render = pygame.font.SysFont("arial.ttf", font_size).render(text, True, color)
+		self.font = pygame.font.SysFont("arial.ttf", font_size)
+		self.render = self.font.render(text, True, color)
 		self.rect = self.render.get_rect()
+		self.color = color
 
 		self.set_position(x, y)
+	
+	def set_text(self, text):
+		self.render = self.font.render(text, True, self.color)
 	
 	def set_position(self, x, y):
 		self.rect.x = x
