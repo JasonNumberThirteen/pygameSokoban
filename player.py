cf56418fb66fdfc4bd8eb73eb27f30ea64998d89
Jason <numberthirteen4026@gmail.com>
2023-02-22 14:29:13 +0100

    Add a moves counter to the player

--- player.py
+++ player.py
@@ -5,17 +5,22 @@ class Player(object):
 	def __init__(self, x, y):
 		self.x = x
 		self.y = y
+		self.moves = 0
 	
 	def detect_input(self, event):
 		if event.type == pygame.KEYDOWN:
 			if event.key == ord("w"):
 				self.y -= constants.TILE_HEIGHT
+				self.moves += 1
 			elif event.key == ord("s"):
 				self.y += constants.TILE_HEIGHT
+				self.moves += 1
 			elif event.key == ord("a"):
 				self.x -= constants.TILE_WIDTH
+				self.moves += 1
 			elif event.key == ord("d"):
 				self.x += constants.TILE_WIDTH
+				self.moves += 1
 	
 	def draw(self, surface):
 		x = self.x + constants.TILE_WIDTH // 2
