482ffaa8fb879841265ca3a848b2daaa337addaa
Jason <numberthirteen4026@gmail.com>
2023-02-26 19:58:56 +0100

    Add inheritances from the ShadedRectangularObject class

--- box.py
+++ box.py
@@ -1,25 +1,21 @@
-import pygame
-import rectangular_object
+import shaded_rectangular_object
 
 from constants import (TILE_WIDTH, TILE_HEIGHT, BOX_WIDTH, BOX_HEIGHT, BOX_COLOR, BOX_BORDER_RADIUS, BOX_SHADE_COLOR, BOX_SHADE_OFFSET)
 
-class Box(rectangular_object.RectangularObject):
+class Box(shaded_rectangular_object.ShadedRectangularObject):
 	def __init__(self, x, y):
-		super().__init__(x, y, BOX_WIDTH, BOX_HEIGHT, BOX_COLOR, 0, BOX_BORDER_RADIUS)
+		super().__init__(x, y, BOX_WIDTH, BOX_HEIGHT, BOX_COLOR, 0, BOX_BORDER_RADIUS, BOX_SHADE_COLOR)
 		
 		self.is_in_slot = False
 	
-	def draw(self, surface):
-		rect = pygame.Rect(self.x*TILE_WIDTH + self.offset_x(), self.y*TILE_HEIGHT + self.offset_y() + BOX_SHADE_OFFSET, self.width, self.height)
-		
-		pygame.draw.rect(surface, BOX_SHADE_COLOR, rect, 0, BOX_BORDER_RADIUS)
-		super().draw(surface)
-
 	def offset_x(self):
 		return (TILE_WIDTH - BOX_WIDTH) // 2
 
 	def offset_y(self):
 		return (TILE_HEIGHT - BOX_WIDTH) // 2
+	
+	def shade_offset(self):
+		return BOX_SHADE_OFFSET
 	
 	def can_be_moved(self, level, offset_x, offset_y):
 		for t in level.tiles:
--- tile.py
+++ tile.py
@@ -1,17 +1,13 @@
-import pygame
-import rectangular_object
+import shaded_rectangular_object
 
 from constants import (TILE_WIDTH, TILE_HEIGHT, TILE_OFFSET, TILE_COLOR, TILE_BORDER_RADIUS, TILE_SHADE_COLOR, TILE_SHADE_OFFSET)
 
-class Tile(rectangular_object.RectangularObject):
+class Tile(shaded_rectangular_object.ShadedRectangularObject):
 	def __init__(self, x, y):
-		super().__init__(x, y, TILE_WIDTH - TILE_OFFSET, TILE_HEIGHT - TILE_OFFSET, TILE_COLOR, 0, TILE_BORDER_RADIUS)
-	
-	def draw(self, surface):
-		rect = pygame.Rect(self.x*TILE_WIDTH + self.offset_x(), self.y*TILE_HEIGHT + self.offset_y() + TILE_SHADE_OFFSET, self.width, self.height)
-		
-		pygame.draw.rect(surface, TILE_SHADE_COLOR, rect, 0, TILE_BORDER_RADIUS)
-		super().draw(surface)
+		super().__init__(x, y, TILE_WIDTH - TILE_OFFSET, TILE_HEIGHT - TILE_OFFSET, TILE_COLOR, 0, TILE_BORDER_RADIUS, TILE_SHADE_COLOR)
 	
 	def offset_x(self):
 		return TILE_OFFSET
+
+	def shade_offset(self):
+		return TILE_SHADE_OFFSET
