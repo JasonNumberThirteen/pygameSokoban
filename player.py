70acb43210148063862f96bfd2db77ba062ccabe
Jason <numberthirteen4026@gmail.com>
2023-02-26 13:41:51 +0100

    Split player movement conditions into separate methods

--- player.py
+++ player.py
@@ -9,18 +9,30 @@ class Player(object):
 	
 	def detect_input(self, event):
 		if event.type == pygame.KEYDOWN:
-			if event.key == ord("w"):
+			if self.can_move_up(event):
 				self.y -= constants.TILE_HEIGHT
 				self.moves += 1
-			elif event.key == ord("s"):
+			elif self.can_move_down(event):
 				self.y += constants.TILE_HEIGHT
 				self.moves += 1
-			elif event.key == ord("a"):
+			elif self.can_move_left(event):
 				self.x -= constants.TILE_WIDTH
 				self.moves += 1
-			elif event.key == ord("d"):
+			elif self.can_move_right(event):
 				self.x += constants.TILE_WIDTH
 				self.moves += 1
+	
+	def can_move_up(self, event):
+		return event.key == ord("w")
+	
+	def can_move_down(self, event):
+		return event.key == ord("s")
+	
+	def can_move_left(self, event):
+		return event.key == ord("a")
+	
+	def can_move_right(self, event):
+		return event.key == ord("d")
 	
 	def draw(self, surface):
 		x = self.x + constants.TILE_WIDTH // 2
