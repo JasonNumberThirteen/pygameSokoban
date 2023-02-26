1198c1b9bc538bcf55286ae5135bc7dd224a1880
Jason <numberthirteen4026@gmail.com>
2023-02-26 19:57:02 +0100

    Add an attribute for shade color

--- shaded_rectangular_object.py
+++ shaded_rectangular_object.py
@@ -4,10 +4,15 @@ import rectangular_object
 from constants import (TILE_WIDTH, TILE_HEIGHT)
 
 class ShadedRectangularObject(rectangular_object.RectangularObject):
+	def __init__(self, x, y, width, height, color, frame_width, border_radius, shade_color):
+		super().__init__(x, y, width, height, color, frame_width, border_radius)
+
+		self.shade_color = shade_color
+	
 	def draw(self, surface):
 		rect = pygame.Rect(self.x*TILE_WIDTH + self.offset_x(), self.y*TILE_HEIGHT + self.offset_y() + self.shade_offset(), self.width, self.height)
 		
-		pygame.draw.rect(surface, self.color, rect, 0, self.border_radius)
+		pygame.draw.rect(surface, self.shade_color, rect, 0, self.border_radius)
 		super().draw(surface)
 	
 	def shade_offset(self):
