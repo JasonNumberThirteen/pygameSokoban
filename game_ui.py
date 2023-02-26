bd9abd6dd50655a5f3e63cb6993e4c572861f311
Jason <numberthirteen4026@gmail.com>
2023-02-26 14:24:47 +0100

    Add a constant for game width

--- game_ui.py
+++ game_ui.py
@@ -1,11 +1,12 @@
 import text
+import constants
 
 class GameUI(object):
 	def __init__(self, moves, level_number):
 		self.level_counter = text.Text("LEVEL " + str(level_number), 4, 4, (32, 32, 32))
 		self.moves_counter = text.Text("Moves: " + str(moves), 4, 4, (32, 32, 32))
 
-		self.moves_counter.rect.right = 600
+		self.moves_counter.rect.right = constants.GAME_WIDTH - 32
 
 	def draw(self, surface):
 		self.level_counter.draw(surface)
--- sokoban.py
+++ sokoban.py
@@ -1,7 +1,8 @@
 import game
+import constants
 
 def main():
-	game_instance = game.Game(640, 480, "Sokoban")
+	game_instance = game.Game(constants.GAME_WIDTH, 480, "Sokoban")
 
 if __name__ == "__main__":
 	main()
