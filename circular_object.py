d17e37bd84c32c52d58155ec4bc124d42fa67470
Jason <numberthirteen4026@gmail.com>
2023-02-28 14:02:05 +0100

    Add a class for circular object

---
+++ circular_object.py
@@ -0,0 +1,24 @@
+import point
+import pygame
+
+from constants import (TILE_WIDTH, TILE_HEIGHT)
+
+class CircularObject(point.Point):
+	def __init__(self, x, y, width, height, color, border_radius):
+		super().__init__(x, y)
+
+		self.width = width
+		self.height = height
+		self.color = color
+		self.border_radius = border_radius
+	
+	def draw(self, surface):
+		position = (self.x*TILE_WIDTH + self.offset_x(), self.y*TILE_HEIGHT + self.offset_y())
+		
+		pygame.draw.circle(surface, self.color, position, self.border_radius)
+
+	def offset_x(self):
+		return 0
+	
+	def offset_y(self):
+		return 0
