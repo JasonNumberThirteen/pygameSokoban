d9dbad09f8a90d8f72adce6f6526c1b1be5f23ac
Jason <numberthirteen4026@gmail.com>
2023-02-28 14:15:09 +0100

    Add inheritances from the GameObject class

--- circular_object.py
+++ circular_object.py
@@ -1,9 +1,9 @@
-import point
 import pygame
+import game_object
 
 from constants import (TILE_WIDTH, TILE_HEIGHT)
 
-class CircularObject(point.Point):
+class CircularObject(game_object.GameObject):
 	def __init__(self, x, y, color, border_radius):
 		super().__init__(x, y)
 
@@ -14,9 +14,3 @@ class CircularObject(point.Point):
 		position = (self.x*TILE_WIDTH + self.offset_x(), self.y*TILE_HEIGHT + self.offset_y())
 		
 		pygame.draw.circle(surface, self.color, position, self.border_radius)
-
-	def offset_x(self):
-		return 0
-	
-	def offset_y(self):
-		return 0
--- rectangular_object.py
+++ rectangular_object.py
@@ -1,9 +1,9 @@
-import point
 import pygame
+import game_object
 
 from constants import (TILE_WIDTH, TILE_HEIGHT)
 
-class RectangularObject(point.Point):
+class RectangularObject(game_object.GameObject):
 	def __init__(self, x, y, width, height, color, frame_width, border_radius):
 		super().__init__(x, y)
 
@@ -17,9 +17,3 @@ class RectangularObject(point.Point):
 		rect = pygame.Rect(self.x*TILE_WIDTH + self.offset_x(), self.y*TILE_HEIGHT + self.offset_y(), self.width, self.height)
 		
 		pygame.draw.rect(surface, self.color, rect, self.frame_width, self.border_radius)
-
-	def offset_x(self):
-		return 0
-	
-	def offset_y(self):
-		return 0
