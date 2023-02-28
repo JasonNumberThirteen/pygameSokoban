4a07549009c434419b2deb2fd2eeb00a18b3fffe
Jason <numberthirteen4026@gmail.com>
2023-02-28 14:31:14 +0100

    Add a class for moves counter

---
+++ moves_counter.py
@@ -0,0 +1,12 @@
+import text
+
+from constants import (GAME_WIDTH, MOVES_TEXT)
+
+class MovesCounter(text.Text):
+	def __init__(self, moves):
+		super().__init__(MOVES_TEXT + ": " + str(moves), 4, 4)
+	
+	def update(self, value):
+		self.set_text(MOVES_TEXT + ": " + str(value))
+
+		self.rect.right = GAME_WIDTH
