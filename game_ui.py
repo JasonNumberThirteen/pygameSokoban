bede1b09cb340c427507b575b686becf6bf3921e
Jason <numberthirteen4026@gmail.com>
2023-03-01 15:57:40 +0100

    Add press any key text to the UI

--- game_ui.py
+++ game_ui.py
@@ -1,6 +1,7 @@
 import level_counter
 import moves_counter
 import boxes_counter
+import press_any_key_text
 import level_complete_text
 
 class GameUI(object):
@@ -10,12 +11,14 @@ class GameUI(object):
 		self.moves_counter = moves_counter.MovesCounter(gm.level.player.moves)
 		self.boxes_counter = boxes_counter.BoxesCounter(gm.level)
 		self.level_complete_text = level_complete_text.LevelCompleteText(gm)
+		self.press_any_key_text = press_any_key_text.PressAnyKeyText(gm)
 
 	def draw(self, surface):
 		self.level_counter.draw(surface)
 		self.moves_counter.draw(surface)
 		self.boxes_counter.draw(surface)
 		self.level_complete_text.draw(surface)
+		self.press_any_key_text.draw(surface)
 	
 	def update_on_start(self):
 		self.level_counter.update(self.gm.level_number)
