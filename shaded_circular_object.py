ba74c2be9e8c0b9f8b0eaa42356d9ef93065e54b
Jason <numberthirteen4026@gmail.com>
2023-03-01 16:35:59 +0100

    Add antialiasing to circular objects

--- shaded_circular_object.py
+++ shaded_circular_object.py
@@ -1,4 +1,5 @@
 import pygame
+import pygame.gfxdraw
 import circular_object
 
 from constants import (TILE_WIDTH, TILE_HEIGHT)
@@ -12,7 +13,8 @@ class ShadedCircularObject(circular_object.CircularObject):
 	def draw(self, surface):
 		position = (self.x*TILE_WIDTH + self.offset_x(), self.y*TILE_HEIGHT + self.offset_y() + self.shade_offset())
 		
-		pygame.draw.circle(surface, self.shade_color, position, self.border_radius)
+		pygame.gfxdraw.aacircle(surface, position[0], position[1], self.border_radius - 1, self.shade_color)
+		pygame.gfxdraw.filled_circle(surface, position[0], position[1], self.border_radius - 1, self.shade_color)
 		super().draw(surface)
 	
 	def shade_offset(self):
