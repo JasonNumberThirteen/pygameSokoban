8f6db2d12e0e903ae8ba43ea46b5eb49ec7e2784
Jason <numberthirteen4026@gmail.com>
2023-03-01 15:56:02 +0100

    Add a class for press any key text

---
+++ press_any_key_text.py
@@ -0,0 +1,14 @@
+import text
+
+from constants import (GAME_WIDTH, GAME_HEIGHT, UI_TEXT_COLOR, UI_TEXT_FONT_SIZE)
+
+class PressAnyKeyText(text.Text):
+	def __init__(self, gm):
+		super().__init__("Press any key to continue", 0, 0, UI_TEXT_COLOR, UI_TEXT_FONT_SIZE)
+
+		self.rect = self.render.get_rect(center=(GAME_WIDTH // 2, GAME_HEIGHT))
+		self.gm = gm
+	
+	def draw(self, surface):
+		if self.gm.completed_level():
+			super().draw(surface)
