3ebb711100eb4ba3b4e3ac90fdf741fccf0a6828
Jason <numberthirteen4026@gmail.com>
2023-05-17 13:32:10 +0200

    Add a method for boxes counter text

--- src/ui/boxes_counter.py
+++ src/ui/boxes_counter.py
@@ -4,9 +4,15 @@ from src.constants import (GAME_WIDTH, BOXES_TEXT)
 
 class BoxesCounter(text.Text):
 	def __init__(self, level):
-		super().__init__(BOXES_TEXT + ": " + str(level.inserted_boxes()) + "/" + str(len(level.boxes)), 4, 4)
+		super().__init__(self.text_string(level), 4, 4)
 	
 	def update(self, level):
-		self.set_text(BOXES_TEXT + ": " + str(level.inserted_boxes()) + "/" + str(len(level.boxes)))
+		self.set_text(self.text_string(level))
 
 		self.rect = self.render.get_rect(centerx=GAME_WIDTH // 2)
+	
+	def text_string(self, level):
+		inserted_boxes = str(level.inserted_boxes())
+		all_boxes = str(len(level.boxes))
+		
+		return BOXES_TEXT + ": " + inserted_boxes + "/" + all_boxes
