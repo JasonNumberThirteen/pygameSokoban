5fe4c113521c83dea8cf9924631741e0bc8e8343
Jason <numberthirteen4026@gmail.com>
2023-05-18 17:46:01 +0200

    Refactor code in the PressAnyKeyText class

--- src/ui/press_any_key_text.py
+++ src/ui/press_any_key_text.py
@@ -1,12 +1,12 @@
 import src.ui.text as text
 
-from src.constants import (GAME_WIDTH, GAME_HEIGHT, UI_TEXT_COLOR, UI_TEXT_FONT_SIZE)
+from src.constants import (GAME_WIDTH, GAME_HEIGHT)
 
 class PressAnyKeyText(text.Text):
 	def __init__(self, gm):
-		super().__init__("Press any key to continue", 0, 0, UI_TEXT_COLOR, UI_TEXT_FONT_SIZE)
-
-		self.rect = self.render.get_rect(centerx=GAME_WIDTH // 2, bottom=GAME_HEIGHT)
+		super().__init__("")
+		self.set_text("Press any key to continue", centerx=GAME_WIDTH // 2, bottom=GAME_HEIGHT)
+		
 		self.gm = gm
 	
 	def draw(self, surface):
