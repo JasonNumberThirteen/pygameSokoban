acd73d87fecc55a2f98fc590980d5d040d0f6129
Jason <numberthirteen4026@gmail.com>
2023-05-18 17:10:30 +0200

    Add a class for player input

--- src/game_objects/player.py
+++ src/game_objects/player.py
@@ -1,54 +1,20 @@
 import pygame
 import src.game_objects.point as point
+import src.game_objects.player_input as player_input
 import src.game_objects.shaded_circular_object as shaded_circular_object
 
-from src.constants import (PLAYER_UP_MOVEMENT_KEY, PLAYER_DOWN_MOVEMENT_KEY, PLAYER_LEFT_MOVEMENT_KEY, PLAYER_RIGHT_MOVEMENT_KEY, LEVEL_RESTART_KEY, TILE_WIDTH, TILE_HEIGHT, PLAYER_COLOR, PLAYER_RADIUS, PLAYER_SHADE_COLOR, PLAYER_SHADE_OFFSET)
+from src.constants import (TILE_WIDTH, TILE_HEIGHT, PLAYER_COLOR, PLAYER_RADIUS, PLAYER_SHADE_COLOR, PLAYER_SHADE_OFFSET)
 
 class Player(shaded_circular_object.ShadedCircularObject):
 	def __init__(self, gm, x, y):
 		super().__init__(x, y, PLAYER_COLOR, PLAYER_RADIUS, PLAYER_SHADE_COLOR)
 		
-		self.gm = gm
 		self.moves = 0
-		self.input_callbacks = {PLAYER_UP_MOVEMENT_KEY: self.on_up_press, PLAYER_DOWN_MOVEMENT_KEY: self.on_down_press, PLAYER_LEFT_MOVEMENT_KEY: self.on_left_press, PLAYER_RIGHT_MOVEMENT_KEY: self.on_right_press}
+		self.input = player_input.PlayerInput(self, gm)
 	
 	def detect_input(self, event, level, ui):
 		if event.type == pygame.KEYDOWN:
-			for k, v in self.input_callbacks.items():
-				if self.pressed_key(event, k):
-					v(level, ui)
-	
-	def on_up_press(self, level, ui):
-		if self.can_move_to(level, 0, -1):
-			self.y -= 1
-
-			self.move_box(level, 0, -1)
-			self.gm.on_player_move(self)
-			ui.on_player_move()
-	
-	def on_down_press(self, level, ui):
-		if self.can_move_to(level, 0, 1):
-			self.y += 1
-
-			self.move_box(level, 0, 1)
-			self.gm.on_player_move(self)
-			ui.on_player_move()
-	
-	def on_left_press(self, level, ui):
-		if self.can_move_to(level, -1, 0):
-			self.x -= 1
-
-			self.move_box(level, -1, 0)
-			self.gm.on_player_move(self)
-			ui.on_player_move()
-	
-	def on_right_press(self, level, ui):
-		if self.can_move_to(level, 1, 0):
-			self.x += 1
-
-			self.move_box(level, 1, 0)
-			self.gm.on_player_move(self)
-			ui.on_player_move()
+			self.input.detect_input(event, level, ui)
 	
 	def can_move_to(self, level, offset_x, offset_y):
 		return level.tile_is_walkable(self, point.Point(offset_x, offset_y))
@@ -57,12 +23,6 @@ class Player(shaded_circular_object.ShadedCircularObject):
 		for b in level.boxes:
 			if self.has_the_same_position(b):
 				b.move(level, offset_x, offset_y)
-	
-	def can_reset_level(self, event):
-		return self.moves > 0 and self.pressed_key(event, LEVEL_RESTART_KEY)
-	
-	def pressed_key(self, event, key):
-		return event.key == ord(key)
 	
 	def offset_x(self):
 		return TILE_WIDTH // 2
---
+++ src/game_objects/player_input.py
@@ -0,0 +1,50 @@
+from src.constants import (PLAYER_UP_MOVEMENT_KEY, PLAYER_DOWN_MOVEMENT_KEY, PLAYER_LEFT_MOVEMENT_KEY, PLAYER_RIGHT_MOVEMENT_KEY, LEVEL_RESTART_KEY)
+
+class PlayerInput():
+	def __init__(self, player, gm) -> None:
+		self.gm = gm
+		self.player = player
+		self.callbacks = {PLAYER_UP_MOVEMENT_KEY: self.on_up_press, PLAYER_DOWN_MOVEMENT_KEY: self.on_down_press, PLAYER_LEFT_MOVEMENT_KEY: self.on_left_press, PLAYER_RIGHT_MOVEMENT_KEY: self.on_right_press}
+	
+	def detect_input(self, event, level, ui):
+		for k, v in self.callbacks.items():
+			if self.pressed_key(event, k):
+				v(level, ui)
+
+	def on_up_press(self, level, ui):
+		if self.player.can_move_to(level, 0, -1):
+			self.player.y -= 1
+
+			self.player.move_box(level, 0, -1)
+			self.gm.on_player_move(self.player)
+			ui.on_player_move()
+	
+	def on_down_press(self, level, ui):
+		if self.player.can_move_to(level, 0, 1):
+			self.player.y += 1
+
+			self.player.move_box(level, 0, 1)
+			self.gm.on_player_move(self.player)
+			ui.on_player_move()
+	
+	def on_left_press(self, level, ui):
+		if self.player.can_move_to(level, -1, 0):
+			self.player.x -= 1
+
+			self.player.move_box(level, -1, 0)
+			self.gm.on_player_move(self.player)
+			ui.on_player_move()
+	
+	def on_right_press(self, level, ui):
+		if self.player.can_move_to(level, 1, 0):
+			self.player.x += 1
+
+			self.player.move_box(level, 1, 0)
+			self.gm.on_player_move(self.player)
+			ui.on_player_move()
+	
+	def can_reset_level(self, event):
+		return self.player.moves > 0 and self.pressed_key(event, LEVEL_RESTART_KEY)
+	
+	def pressed_key(self, event, key):
+		return event.key == ord(key)
